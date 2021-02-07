5e0de38a234a736e55d1aeeab3edb3e152fe9123
Maria Philna <ced17i028@iiitdm.ac.in>
2021-02-07 19:19:19 +0530

    Execise 3

--- LAB1/q3/q3.sh
+++ LAB1/q3/q3.sh
@@ -2,8 +2,8 @@
 echo Enter url
 read url
 
-# curl to get the main html file
+# curl is used to get the main html file
 time curl $url > webpage.html
 
-# wget to obtain all the necessary resources
+# wget is used to obtain all the necessary resources
 wget -r -np -k $url 
