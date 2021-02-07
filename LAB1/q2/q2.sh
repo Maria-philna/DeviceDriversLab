199366eb8166fc297e5eab2edefbd89daf6ed053
Maria Philna <ced17i028@iiitdm.ac.in>
2021-02-07 14:28:30 +0530

    Exercise 2

---
+++ LAB1/q2/q2.sh
@@ -0,0 +1,34 @@
+#A shell script to input two numbers and perform arithmentic operation, comparison operations.
+echo "Enter first number"
+read x
+echo "Enter second number"
+read y
+(( sum =x+y ))
+echo ""
+echo "Results of Arithmetic operations"
+echo "The result of addition=$sum"
+(( diff =x-y ))
+echo "The result of subtraction=$diff"
+(( product =x*y ))
+echo "The result of product=$product"
+(( div =x/y ))
+echo "The result of division=$div"
+(( pow1 =x**y ))
+echo "The result of x raised to the power of y=$pow1"
+(( pow2 =y**x ))
+echo "The result of y raised to the power of x=$pow2"
+echo ""
+echo "Results of comparison operations"
+if [ $x == $y ]
+then
+   echo "x is equal to y"
+elif [ $x != $y ]
+then
+   echo "x is not equal to y"
+	if [ $x > $y ]
+	then
+   		echo "x is greater than y"
+	else
+   		echo "x is less than y"
+	fi
+fi
