92f33ac151542b30a97d28fa2c04d57fd3248eb0
Maria-philna <66329480+Maria-philna@users.noreply.github.com>
2021-04-12 19:46:54 +0530

    Delete .keylogger.ko.cmd

--- LAB4/Exercise9/.keylogger.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/maria/LAB4/Exercise9/keylogger.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/maria/LAB4/Exercise9/keylogger.ko /home/maria/LAB4/Exercise9/keylogger.o /home/maria/LAB4/Exercise9/keylogger.mod.o ;  true
