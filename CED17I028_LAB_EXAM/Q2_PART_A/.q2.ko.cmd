11544ecb87b26de9c447aa988aa5459c0a2e0a22
Maria Philna <ced17i028@iiitdm.ac.in>
2021-04-15 12:04:26 +0530

    Lab Exam

---
+++ CED17I028_LAB_EXAM/Q2_PART_A/.q2.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/maria/college/CED17I028_LAB_EXAM/Q2_PART_A/q2.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/maria/college/CED17I028_LAB_EXAM/Q2_PART_A/q2.ko /home/maria/college/CED17I028_LAB_EXAM/Q2_PART_A/q2.o /home/maria/college/CED17I028_LAB_EXAM/Q2_PART_A/q2.mod.o ;  true
---
+++ CED17I028_LAB_EXAM/Q2_PART_B/.tmp_versions/q2.mod
@@ -0,0 +1,3 @@
+/home/maria/college/CED17I028_LAB_EXAM/Q2_PART_B/q2.ko
+/home/maria/college/CED17I028_LAB_EXAM/Q2_PART_B/q2.o
+
